b4c2c32630249a24de6b122aa0aa4cc7eeebc2c9
baledick <vasilvalat@gmail.com>
2019-12-26 23:57:44 +0200

    Update and rename Integral.c to C/Integral.c

--- C/Integral.c
+++ C/Integral.c
@@ -12,10 +12,10 @@ int main(){
 	while(f>e){
 		z=pow(-1,(i+2));
 		Prodn=Prod*pow((5*3.14)+1/(2*(i+1)), (z*(i+1)));
-		printf("O %d oros einai o: %lf \n", i, Prod);
+		printf("The %d th term is: %lf \n", i, Prod);
 		Prod=Prodn;
 		fn=f/i;
-		printf("I apostasi apo to e einai %lf \n", f-e);
+		printf("Distance from e is %lf \n", f-e);
 		f=fn;
 		
 		
