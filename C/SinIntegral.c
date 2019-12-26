5ffea5231d09e83f730aa89bbdca5e8178e114bb
baledick <vasilvalat@gmail.com>
2019-12-27 00:06:12 +0200

    Update and rename SinIntegral.c to C/SinIntegral.c

--- C/SinIntegral.c
+++ C/SinIntegral.c
@@ -4,13 +4,13 @@
 int main(){
 	int n, i;
 	double a, b, sum, h, ex;
-	printf("Dose to kato akro ");
+	printf("Give me the lower bound ");
 	scanf("%lf", &a);
 	printf("%lf\n", a);
-	printf("Dwse kai to panw ");
+	printf("Now for the upper bound ");
 	scanf("%lf", &b);
 	printf("%lf\n", b);
-	printf("Kai ton arithmo twn vimatwn ");
+	printf("And finally, the number of steps ");
 	scanf("%d", &n);
 	printf("%d\n", n);
 	sum=0.0;
