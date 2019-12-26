51f4ab7a3c92f8f1e9c25cfd3e86cb3a473a782d
baledick <vasilvalat@gmail.com>
2019-12-26 23:54:53 +0200

    Update and rename FileMatrix.c to C/FileMatrix.c

--- C/FileMatrix.c
+++ C/FileMatrix.c
@@ -6,14 +6,14 @@
 int main(){
 	FILE *fp;
 	int n, i, j;
-	printf("Dwse n ");
+	printf("Give the dimensions n ");
 	scanf("%d", &n);
 	
 	int a[n][n], b[n][n];
 	fp = fopen("kopa.kabana", "w");
 	for(i=0; i<n; i++){
 		for(j=0; j<n; j++){
-			printf("Dwse tin timi tou pinaka a stin thesi [%d][%d]", i,j);
+			printf("Give the value of matrix a in [%d][%d]", i,j);
 			scanf("%d", &a[i][j]);
 			fprintf(fp, "%d ", a[i][j]);
 		}
@@ -23,7 +23,7 @@ int main(){
 	for(i=0; i<n; i++){
 		fprintf(fp, "\t");
 		for(j=0; j<n; j++){
-			printf("Dwse tin timi tou pinaka b stin thesi [%d][%d]", i,j);
+			printf("Place the value of matrix b in [%d][%d]", i,j);
 			scanf("%d", &b[i][j]);
 			fprintf(fp, " %d", b[i][j]);
 		}
