2c0aadaef67c1c674ccfa5c0a84427831b90c8f8
Vasilis Valatsos <vasilvalat@gmail.com>
2020-06-29 21:42:26 +0300

    Started solving the final exam

---
+++ Solutions/Exam_Sol1.py
@@ -0,0 +1,15 @@
+def function(x):
+    y = x - 2**(0.33)
+    return y
+
+def newtonian_function(x):
+    for n in range(3):
+        x -= function(x)
+    return x
+
+newtonian_function(2)
+
+root = 1.259921
+approximation = round(newtonian_function(2), 6)
+
+error_perchentage = (1 - approximation/root)*100)
