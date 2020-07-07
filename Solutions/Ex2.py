f29f612b9ae87803efa0f63f1b34c30c321386e2
Vasilis Valatsos <vasilvalat@gmail.com>
2020-07-07 14:52:57 +0300

    updated it so it better reflects the question.

--- Solutions/Ex2.py
+++ Solutions/Ex2.py
@@ -4,18 +4,18 @@ def integral(x):
     y = x**2
     return y
 
-n = 100000
 n0 = 0 # this is the integral
 n1 = 0 # this is the deviation
-a = 1
-b = 11
+a=1
+b=11
 
-for i in range(n):
-    rand = np.random.uniform(a,b)
+array = [5, 9, 4]
+for i in range(3):
+    rand = array[i]
     n0 += integral(rand)
     n1 += integral(rand)**2
 
-int = (b-a)*n0/n
-sigma = ((n1/n) - ((n0/n)**2))**0.5
-dI = sigma/(n**0.5)
+int = (b-a)*n0/3
+sigma = ((n1/3) - ((n0/3)**2))**0.5
+dI = sigma/(3**0.5)
 print(int, "+/-", dI)
