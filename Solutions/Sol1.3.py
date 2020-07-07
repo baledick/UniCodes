43b1807c28ecc1ae8f2a45d7e5374978cfb115bd
Vasilis Valatsos <vasilvalat@gmail.com>
2020-07-07 18:06:01 +0300

    added last solution from ex1.pdf

---
+++ Solutions/Sol1.3.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x = np.zeros(3)
+
+A = [[2, 2, 3], [-1, -3, 0], [1,2,1]]
+b = [3, 2, 0]
+e = 3
+for i in range(e): #Gauss-Jacobi
+    m = x[0]
+    n = x[1]
+    p = x[2]
+    x[0] = (3-2*n-3*p)/2
+    x[1] = -(2 + m)/3
+    x[2] = -(m+ 2*n)
+print(x)
+
+for i in range(e): #Gauss-Seidel
+    x[0] = (3-2*x[1]-3*x[2])/2
+    x[1] = -(2 + x[0])/3
+    x[2] = -(x[0]+ 2*x[1])
+print(x)
